3c65175ae004e211e11a3cf68c8f1dfc26df3d5f
Azuatic <94387725+Azuatic@users.noreply.github.com>
2022-10-08 13:03:48 -0300

    Update hitboxextender.lua

---
+++ hitboxextender.lua
@@ -0,0 +1,32 @@
+--// Variables
+game:GetService("StarterGui"):SetCore("SendNotification", {Title = "yeet", Text = "t = Enabled : p = disabled."})
+local userInputService = game:GetService("UserInputService")
+
+--// Main Script
+function core(v)
+    _G.HeadSize = 7
+
+    userInputService.InputBegan:Connect(function(input, chatting)
+        if input.KeyCode == Enum.KeyCode.T then
+            v.Character.HumanoidRootPart.Size = Vector3.new(_G.HeadSize,_G.HeadSize,_G.HeadSize)
+            v.Character.HumanoidRootPart.Transparency = 0.7
+            v.Character.HumanoidRootPart.BrickColor = BrickColor.new("Really blue")
+            v.Character.HumanoidRootPart.Material = "Neon"
+            v.Character.HumanoidRootPart.CanCollide = false
+        elseif input.KeyCode == Enum.KeyCode.P then
+            v.Character.HumanoidRootPart.Size = Vector3.new(2, 2, 1)
+            v.Character.HumanoidRootPart.Transparency = 1
+            v.Character.HumanoidRootPart.BrickColor = BrickColor.new("Medium stone grey")
+            v.Character.HumanoidRootPart.Material = "Plastic"
+            v.Character.HumanoidRootPart.CanCollide = true
+        end
+    end)
+end
+
+for i,v in pairs(game:GetService("Players"):GetPlayers()) do
+    if v.Name ~= game.Players.LocalPlayer.Name then
+        core(v)
+    end
+end
+
+game.Players.PlayerAdded:Connect(core)
